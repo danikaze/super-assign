93b3055c4d90883a4f08907e52c9646f628c5f56
danikaze <danikaze@gmail.com>
2022-12-02 23:58:13 +0900

    super-assign package implementation

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,110 @@
-export function foobar(): void {
-  // tslint:disable-next-line: no-console
-  console.log('hello world');
+export interface AssignOptions {
+  /** List of values to ignore  */
+  ignoreValues?: any[];
+  /** List of keys to ignore */
+  ignoreKeys?: any[];
+  /** Value used to delete object keys  */
+  deleteValue?: any;
+  /**
+   * When `true`, the returned value will be a new object
+   * and the target(first argument) will not be modified
+   */
+  returnCopy: boolean;
+  /** When `true` no deep assignments will be done */
+  shallow: boolean;
+}
+
+export const ASSIGN_DELETE = Symbol('ASSIGN_DELETE');
+
+export const ASSIGN_DEFAULT_OPTIONS: AssignOptions = {
+  ignoreValues: [undefined],
+  deleteValue: ASSIGN_DELETE,
+  returnCopy: false,
+  shallow: false,
+};
+
+export function getCustomAssign(options: Partial<AssignOptions> = {}): Assign {
+  return assignWithOptions.bind(undefined, {
+    ...ASSIGN_DEFAULT_OPTIONS,
+    ...options,
+  }) as unknown as Assign;
+}
+
+export const assign = getCustomAssign() as Assign;
+
+export const assignCopy = getCustomAssign({ returnCopy: true }) as Assign;
+
+type Assign = <T extends {}, U extends any[]>(
+  target: T,
+  ...args: U
+) => Assigned<T, U>;
+
+type Assigned<T, U extends any[]> = {
+  0: T;
+  1: ((...t: U) => any) extends (head: infer Head, ...tail: infer Tail) => any
+    ? Assigned<Omit<T, keyof Head> & Head, Tail>
+    : never;
+}[U['length'] extends 0 ? 0 : 1];
+
+const checkDom = typeof Element !== 'undefined';
+
+export function assignWithOptions<T extends {}>(
+  options: AssignOptions,
+  ...sources: (T | any)[]
+): T {
+  const { ignoreValues, ignoreKeys, deleteValue, returnCopy, shallow } =
+    options;
+  const target = returnCopy ? {} : (sources[0] as T);
+
+  for (let i = returnCopy ? 0 : 1; i < sources.length; i++) {
+    const source = sources[i];
+
+    // ignore null/undefined source objects
+    if (source == undefined) {
+      continue;
+    }
+
+    // extend the target
+    const keys = Object.keys(source);
+    for (const key of keys) {
+      if (ignoreKeys?.includes(key)) continue;
+
+      const value = source[key as keyof T];
+
+      if (value === target || ignoreValues?.includes(value)) {
+        continue;
+      }
+
+      if (deleteValue !== undefined && deleteValue === value) {
+        delete (target as T)[key as keyof T];
+        continue;
+      }
+
+      if (Array.isArray(value)) {
+        (target as T)[key as keyof T] = value.slice() as T[keyof T];
+        continue;
+      }
+
+      const shouldShallowAssign =
+        shallow ||
+        // functions are always referenced
+        typeof value === 'function' ||
+        // DOM elements are always referenced
+        (checkDom && value instanceof Element) ||
+        // null values are not objects
+        value === null ||
+        typeof value !== 'object';
+      (target as T)[key as keyof T] = (
+        shouldShallowAssign
+          ? value
+          : assignWithOptions(
+              options,
+              (target as T)![key as keyof T] as {},
+              value
+            )
+      ) as T[keyof T];
+    }
+  }
+
+  return target as T;
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,10 +1,151 @@
-import { describe, it } from 'mocha';
-import { assert } from 'chai';
-import { foobar } from '@src';
+import 'jest';
+import { assign, assignCopy, ASSIGN_DELETE, getCustomAssign } from '@src';
 
-describe('#foobar test', () => {
-  it('should not fail ^^', () => {
-    assert.exists(foobar);
-    assert.doesNotThrow(foobar);
+describe('Default assign()', () => {
+  it('should do nothing if there are no values', () => {
+    const base = { a: 1 };
+    expect(assign(base)).toEqual(base);
+    expect(assign(base, null)).toEqual(base);
+    expect(assign(base, undefined)).toEqual(base);
+  });
+
+  it('should extend and modify the target object but not the sources', () => {
+    const base = {
+      num: 1,
+      arr: [1, 2, 3],
+      obj: {
+        foo: 'foo',
+        bar: 'bar',
+        deep: { v: 'hoge' },
+      },
+    };
+    const source1 = { num: 2, foobar: 'hoge' };
+    const source2 = { num: 3 };
+    const source3 = {
+      obj: { val: 'newval', foo: 'foo2', deep: { v2: 12345 } },
+    };
+    const source4 = {
+      // fn: () => {},
+      dom: document.createElement('div'),
+    };
+
+    const res = assign(base, source1, source2, source3, source4);
+    expect(res).toBe(base);
+    expect(res).toEqual(base);
+    expect(res).toEqual({
+      num: 3,
+      arr: [1, 2, 3],
+      obj: {
+        foo: 'foo2',
+        bar: 'bar',
+        deep: { v: 'hoge', v2: 12345 },
+        val: 'newval',
+      },
+      foobar: 'hoge',
+      // fn: () => {},
+      dom: document.createElement('div'),
+    });
+    expect(source1).toEqual({ num: 2, foobar: 'hoge' });
+    expect(source2).toEqual({ num: 3 });
+  });
+
+  it('should shallow copy arrays', () => {
+    const base = { n: 123 };
+    const src = { s: 'foobar', arr: ['a', {}, 0] };
+    const res = assign(base, src);
+
+    expect(res).toEqual({ n: 123, s: 'foobar', arr: ['a', {}, 0] });
+    expect(res.arr).not.toBe(src.arr);
+    expect(res.arr[0]).toBe(src.arr[0]);
+    expect(res.arr[1]).toBe(src.arr[1]);
+    expect(res.arr[2]).toBe(src.arr[2]);
+  });
+
+  it('should just reference functions', () => {
+    const base = { n: 123 };
+    const src = { s: 'foobar', fn: () => {} };
+    const res = assign(base, src);
+    const { fn, ...obj } = res;
+
+    expect(obj).toEqual({ n: 123, s: 'foobar' });
+    expect(fn).toBe(src.fn);
+  });
+
+  it('should just reference DOM elements', () => {
+    const base = { n: 123 };
+    const src = { s: 'foobar', dom: document.createElement('div') };
+    const res = assign(base, src);
+    const { dom, ...obj } = res;
+
+    expect(obj).toEqual({ n: 123, s: 'foobar' });
+    expect(dom).toBe(src.dom);
+  });
+
+  it('should just reference Symbols', () => {
+    const base = { n: 123 };
+    const src = { s: 'foobar', sym: Symbol('xyz') };
+    const res = assign(base, src);
+    const { sym, ...obj } = res;
+
+    expect(obj).toEqual({ n: 123, s: 'foobar' });
+    expect(sym).toBe(src.sym);
+  });
+
+  it('should not modify the target object when the copy option is specified', () => {
+    const base = { num: 1 };
+    const res = assignCopy(base, { str: 'foobar' });
+
+    expect(res).not.toBe(base);
+    expect(base).toEqual({ num: 1 });
+    expect(res).toEqual({ num: 1, str: 'foobar' });
+  });
+
+  it('should ignore specified keys', () => {
+    const customAssign = getCustomAssign({ ignoreKeys: ['ignore'] });
+    const res = customAssign({ base: 1 }, { n: 2, ignore: 3, nil: null });
+    expect(res).toEqual({ base: 1, n: 2, nil: null });
+  });
+
+  it('should ignore specified values', () => {
+    const customAssign = getCustomAssign({ ignoreValues: [undefined, null] });
+    const res1 = customAssign({ base: 1 }, { n: 2, a: null, b: undefined });
+    expect(res1).toEqual({ base: 1, n: 2 });
+
+    // no ignored values
+    const noIgnoreAssign = getCustomAssign({ ignoreValues: undefined });
+    const res2 = noIgnoreAssign({ base: 1 }, { n: 2, a: null, b: undefined });
+    expect(res2).toEqual({ base: 1, n: 2, a: null, b: undefined });
+  });
+
+  it('should apply deletion member if enabled', () => {
+    // default delete value
+    const res1 = assign(
+      { base: 1, del: 2, o: { a: 1, b: 2, c: 3 } },
+      { val: 3, del: ASSIGN_DELETE, o: { a: 0, b: ASSIGN_DELETE } }
+    );
+    expect(res1).toEqual({ base: 1, val: 3, o: { a: 0, c: 3 } });
+
+    // custom delete value
+    const customAssign = getCustomAssign({ deleteValue: null });
+    const res2 = customAssign({ base: 1, del: 2 }, { val: 3, del: null });
+    expect(res2).toEqual({ base: 1, val: 3 });
+
+    // no delete value
+    const noDeleteAssign = getCustomAssign({ deleteValue: undefined });
+    const res3 = noDeleteAssign({ base: 1, del: 2 }, { val: 3, del: null });
+    expect(res3).toEqual({ base: 1, val: 3, del: null });
+  });
+
+  it('should accept shallow assign', () => {
+    const shallowAssign = getCustomAssign({ shallow: true });
+    const base = { a: 1, o: { b: 2, c: 3 } };
+    const src = { s: 'x', o: { d: 4 } };
+    const res = shallowAssign(base, src);
+
+    expect(res).toBe(base);
+    expect(res).toEqual({ a: 1, s: 'x', o: { d: 4 } });
+    expect(src).toEqual({ s: 'x', o: { d: 4 } });
   });
 });
+
+Object.assign({});
